afb9e64bb9bb3753dc6fcc0090ca23a0f42e5e90
maxmaio <maxmaio8@gmail.com>
2022-05-31 15:47:21 -0700

    Initial Commit

---
+++ Tv Storage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tv Storage
+//
+//  Created by Max Maio on 5/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tv Storage/Tv_StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tv_StorageApp.swift
+//  Tv Storage
+//
+//  Created by Max Maio on 5/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Tv_StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
